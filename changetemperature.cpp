2e2a3524593dcc2ff99c01f767e5f1b1af9bcf67
Gurmeet201920 <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-03 20:39:39 +0530

    Add files via upload

---
+++ changetemperature.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float f, c;
+    cout << "enter the value of c" << endl;
+    cin >> c;
+    f = c * (1.800) + 32.00;
+
+    cout << "your temperature is " << f;
+    return 0;
+}
