cc7626985a1ca458a23a63022effcfd2eecc7010
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-04 19:34:33 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "before swapping a = " << a << " and b = " << b << endl;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "after the swapping a = " << a << " and b = " << b << endl;
+    return 0;
+}
