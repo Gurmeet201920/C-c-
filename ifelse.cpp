9a12442d9caad73021ff53c2f0c9eea68924c9d5
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-05 19:24:01 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > b && a > c)
+    {
+        cout << "a is bigger than b and c" << endl;
+    }
+    if (b > a || b > c)
+    {
+        cout << "b is bigger than a and c " << endl;
+    }
+    else
+    {
+        cout << "c is bigger than b and a " << endl;
+    }
+    return 0;
+}
