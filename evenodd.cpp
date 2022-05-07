c534382f0b306c00d626e95a682a243717a14f05
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-07 20:41:04 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "the number is" << endl;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "the number is even." << endl;
+    }
+    if (n % 2 == 1)
+    {
+        cout << "the number is odd." << endl;
+    }
+    return 0;
+}
