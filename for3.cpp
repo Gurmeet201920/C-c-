88674381e3ce6c272c165a4fc6cdb7e7f46f4bdd
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-12 22:01:47 +0530

    Add files via upload

---
+++ for3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter no " << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Gurmeet Singh" << endl;
+    }
+    return 0;
+}
