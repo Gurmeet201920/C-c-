66e8118d2e379c499344aaaa2096457c2bb188cc
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-15 21:44:48 +0530

    Add files via upload

---
+++ foorloop8.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int no, f = 1;
+    cout << "enter any number\n";
+    cin >> no;
+    for (int i = 1; i <= no; i++)
+    {
+        f = f * i;
+    }
+    cout << "factorial=" << f;
+    return 0;
+}
