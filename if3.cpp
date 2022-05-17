e250e6f0a4746ebc6d82d0862a700a48362eb0bc
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-17 08:01:43 +0530

    Add files via upload

---
+++ if3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float p;
+    cout << "enter your percent\n";
+    cin >> p;
+    if (p >= 60)
+        cout << "first division";
+    else if (p >= 45)
+        cout << "second division";
+    else if (p >= 33)
+        cout << "third division";
+    else
+        cout << "fail";
+    return 0;
+}
