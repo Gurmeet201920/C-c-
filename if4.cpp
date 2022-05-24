62f7bdc5c9988687f01938d3336b75640222518d
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-24 20:51:29 +0530

    Add files via upload

---
+++ if4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float p;
+    cout << "enter your percent\n";
+    cin >> p;
+    if (p >= 90)
+        cout << "pass";
+    else
+        cout << "fail";
+    return 0;
+}
