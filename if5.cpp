873330ff4b5d23af6ea52fb1a8382dd36b3cbffa
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-24 21:16:23 +0530

    Add files via upload

---
+++ if5.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float Percent, English, Hindi, Maths, Science, Art;
+    cout << "Enter Marks of English" << endl;
+    cin >> English;
+    cout << "Enter Marks of Hindi" << endl;
+    cin >> Hindi;
+    cout << "Enter Marks of Maths" << endl;
+    cin >> Maths;
+    cout << "Enter Marks of Science" << endl;
+    cin >> Science;
+    cout << "Enter marks of Art" << endl;
+    cin >> Art;
+    Percent = (English + Hindi + Maths + Science + Art) / 5;
+    cout << "Total percent  " << Percent << endl;
+    if (Percent >= 90)
+        cout << "pass" << endl;
+    else
+        cout << " fail";
+
+    return 0;
+}
