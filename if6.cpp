11cc56540b7b31a669c7dd374c150de72ec9b573
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-25 21:46:00 +0530

    Add files via upload

---
+++ if6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number, i;
+    cout << "enter your number" << endl;
+    cin >> number;
+    if (number < 0)
+    {
+        cout << "negative" << endl;
+    }
+    if (number >= 1)
+    {
+        cout << "positive" << endl;
+    }
+    if (number == 0)
+    {
+        cout << "zero" << endl;
+    }
+    return 0;
+}
