970381b4fb4eeb1e7c597653fe37b25e1a3b3b31
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-29 14:40:26 +0530

    Add files via upload

---
+++ intrest.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float p, r, t, si;
+    cout << "enter principle" << endl;
+    cin >> p;
+    cout << "enter rate of interest" << endl;
+    cin >> r;
+    cout << "enter time" << endl;
+    cin >> t;
+    si = (p * r * t) / 100;
+    cout << "simple intrest=" << si;
+    return 0;
+}
