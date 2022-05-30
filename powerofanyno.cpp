7c262eebb17ce0733a4e2ac606f20949571d8d2a
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-05-30 19:27:50 +0530

    Add files via upload

---
+++ powerofanyno.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int b, p, f = 1;
+    cout << "enter base" << endl;
+    cin >> b;
+    cout << "enter power" << endl;
+    cin >> p;
+    for (int i = 1; i <= p; i++)
+    {
+        f = f * b;
+    }
+    cout << "result=" << f;
+    return 0;
+}
