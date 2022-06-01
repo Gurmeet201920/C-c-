0618f28fb522f619465324ccd4106a7428d1980d
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-06-01 21:02:58 +0530

    Add files via upload

---
+++ multiplyintegerno.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int no, b, m = 1;
+    cout << "enter any number" << endl;
+    cin >> no;
+    while (no != 0)
+    {
+        b = no % 10;
+        m = m * b;
+        no = no / 10;
+    }
+    cout << "total multiply of digits=" << m;
+    return 0;
+}
