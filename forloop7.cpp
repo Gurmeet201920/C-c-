842e7fd3be52072dc687149164425fce8cbe517e
Gurmeet singh <104811752+Gurmeet201920@users.noreply.github.com>
2022-06-11 18:13:07 +0530

    Update forloop7.cpp

--- forloop7.cpp
+++ forloop7.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main()
 {
     int no, sum = 0;
-    cout << "enter any numbers\n";
+    cout << "enter any numbers"<<endl;
     cin >> no;
-    cout << "natural no from 1 to " << no << "is given below\n";
+    cout << "natural no from 1 to " << no << "is given below"<<endl;
     for (int i = 1; i <= no; i++)
     {
         cout << i << "";
